0f90a447854f8efe4868155c4d678a0a41ae099c
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-18 18:39:45 +0200

    Add view admin.posts.index

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class=" text-center"> I miei post</h1>
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Scritto il </th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+            @forelse ($posts as $post)
+            <tr>
+                <td>{{$post->title}}</td>
+                <td>{{$post->created_at}}</td>
+                <td><a href="{{route('admin.posts.show',$post->id)}}" class="btn btn-primary">Vai</a></td>
+              </tr>
+            @empty
+              <tr><td colspan="3" class="text-center"> Non ci sono posts al momento</td></tr>
+            @endforelse
+               
+    
+            </tbody>
+          </table>
+    </div>
+@endsection
