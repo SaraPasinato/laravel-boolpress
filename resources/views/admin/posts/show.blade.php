f7f898fa931cc32ac32f6723fca5887ffd10783b
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-18 19:08:27 +0200

    add postController show + fix view tag

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="container-sm">
         <div class="card">
-         <div class="card-title">{{$post->title}}</div>
+         <div class="card-header">
+             <h1 class="card-title text-capitalize">{{$post->title}}</h1>
+         </div>
          <div class="card-body">
              <p class="card-text">{{$post->content}}</p>
              <address>{{$post->getFormattedDate('created_at')}}</address>
