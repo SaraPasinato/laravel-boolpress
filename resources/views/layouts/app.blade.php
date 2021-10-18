4a46fe3a6138e46371252e4d0b5d3442981e345b
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-18 18:45:43 +0200

    add link admin.posts.index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,8 +53,9 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
-
+                            
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{route('admin.posts.index')}}" class="dropdown-item">Vedi Posts</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -66,6 +67,7 @@
                                     </form>
                                 </div>
                             </li>
+                            
                         @endguest
                     </ul>
                 </div>
