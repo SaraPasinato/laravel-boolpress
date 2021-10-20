5c4bd53ba1605050337edeb9d3d0333881aec802
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-20 17:58:30 +0200

    add destroy add btn create, edit,destroy

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,7 +2,15 @@
 
 @section('content')
     <div class="container">
-        <h1 class=" text-center"> I miei post</h1>
+      @if(session('alert-msg'))
+        <div class="alert alert-{{session('alert-type')}}">
+          {{session('alert')}}
+        </div>
+      @endif
+        <header class="my-5 d-flex justify-content-between align-items-center">
+          <h1 class=" text-center"> I miei post</h1>
+          <a href="{{route('admin.posts.create')}}" class="btn btn-success">Nuovo Post</a>
+        </header>
         <table class="table">
             <thead>
               <tr>
@@ -16,7 +24,16 @@
             <tr>
                 <td>{{$post->title}}</td>
                 <td>{{$post->getFormattedDate('created_at')}}</td>
-                <td><a href="{{route('admin.posts.show',$post->id)}}" class="btn btn-primary">Vai</a></td>
+                <td class="d-flex justify-content-center">
+                  <a href="{{route('admin.posts.show',$post->id)}}" class="btn btn-primary mr-2">Vai</a>
+                  <a href="{{route('admin.posts.edit',$post->id)}}" class="btn btn-warning mr-2">Modifica</a>
+                  <form action="{{route('admin.posts.destroy',$post->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Cancella</button>
+                  </form>
+                </td>
+                
               </tr>
             @empty
               <tr><td colspan="3" class="text-center"> Non ci sono posts al momento</td></tr>
