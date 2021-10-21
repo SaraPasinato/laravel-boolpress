eb40052aca175d0096809cf8463b515682e87442
SaraPasinato <pasinatosara.sp@gmail.com>
2021-10-21 13:57:21 +0200

    add css postcard

--- resources/js/components/Posts/PostCard.vue
+++ resources/js/components/Posts/PostCard.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="card">
-  <div class="card-body">
-    <img :src="post.image" :alt="post.title" class="card-img-top">
-    <h5 class="card-title">{{post.title}}</h5>
-    <h6 class="card-subtitle mb-2 text-muted"> pubblicato il {{post.created_at}}</h6>
-    <p class="card-text">{{post.content}}</p>
-  </div>
+  <div class="card w-50 my-2 border border-white">
+    <div class="row h-100 justify-content-between ">
+      <img :src="post.image" :alt="post.title" class="card-img-top col-6">
+      <div class="card-body col-6">
+        <h5 class="card-title">{{post.title}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted"> pubblicato il {{post.created_at}}</h6>
+        <p class="card-text">{{post.content}}</p>
+    </div>
+    </div>
+
 </div>
 </template>
 
@@ -18,7 +21,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card{
-    width: 16rem;
-}
+
 </style>
